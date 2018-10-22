bec549b3f784ab6cfc60ed3186ac02c9df56fec6
Vinesma <Otavio@192.168.1.7>
2018-10-22 11:33:42 -0300

    +commit

---
+++ src/ArvoreBinaria.java
@@ -0,0 +1,54 @@
+public class ArvoreBinaria {
+    Nodulo raiz;
+    
+    private Nodulo addRecursive(Nodulo current, String valor) {
+        if (current == null) {
+            return new Nodulo(valor);
+        }
+ 
+        if (current.esq == null) {
+            current.esq = addRecursive(current.esq, valor);
+        } else if (current.dir == null) {
+            current.dir = addRecursive(current.dir, valor);
+        } else {
+            // valor already exists
+            return current;
+        }
+ 
+        return current;
+    }
+    
+    public void add(String valor) {
+        raiz = addRecursive(raiz, valor);
+    }
+    
+    private Nodulo deleteRecursive(Nodulo current, String valor) {
+        if (current == null) {
+            return null;
+        }
+ 
+        if (valor == current.valor) {
+            // Nodulo to delete found
+            // ... code to delete the node will go here
+            if (current.esq == null && current.dir == null) {
+                return null;
+            }
+            
+            if (current.dir == null) {
+                return current.esq;
+            }
+ 
+            if (current.esq == null) {
+                return current.dir;
+            }
+        }
+        
+        if (current.esq != null) {
+            current.esq = deleteRecursive(current.esq, valor);
+            return current;
+        }
+        
+        current.dir = deleteRecursive(current.dir, valor);
+        return current;
+    }
+}
---
+++ src/Nodulo.java
@@ -0,0 +1,11 @@
+public class Nodulo {
+    String valor;
+    Nodulo esq;
+    Nodulo dir;
+    
+    Nodulo(String valor){        
+        esq = null;
+        this.valor = valor;
+        dir = null;
+    }
+}
