564fa78c6b80a1a57068b5c6f597d94dda7e5f0f
Vinesma <Otavio@192.168.1.7>
2018-11-15 19:25:35 -0300

    +commit

--- src/NovaException.java
+++ src/NovaException.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Otavio
- */
 public class NovaException extends Exception{
     public NovaException(String msg){
         super(msg);
